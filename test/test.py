6a2a563974dc51237deb85d363dde5ea571d4fb2
Tim Zaplavnyy <tima.zaplavnyy@mail.ru>
2024-02-17 11:38:59 +0300

    added tests

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 from src.classes import GetVacancy
 from src.classes import SalaryVacancy
 from src.classes import CertainVacancy
+from src.main import for_usr
 
 import json
 
@@ -28,16 +29,17 @@ def test_get_vacancies():
 def test_sorted_vacancies():
     s = SalaryVacancy("Налоговый инспектор")
     assert s.sorted_salary(2) == [{'id': '93226541',
-            'name': 'Главный государственный налоговый инспектор отдела выездных проверок, предпроверочного анализа',
-            'salary': {'from': 80000, 'to': None, 'currency': 'RUR', 'gross': True},
-            'url': 'https://hh.ru/vacancy/93226541',
-            'description': 'Высшее образование по направлениям подготовки: экономика, бухгалтерский учет, юриспруденция, государственное и муниципальное управление, экономическая безопасность. Опыт работы в ИФНС.',
-            'city': 'Санкт-Петербург'},
-           {'id': '92182049', 'name': 'Старший государственный налоговый инспектор контрольно-аналитического отдела',
-            'salary': {'from': 80000, 'to': 100000, 'currency': 'RUR', 'gross': True},
-            'url': 'https://hh.ru/vacancy/92182049',
-            'description': 'опыт работы в контрольном блоке (КАО, ППА, ВНП, ОКП при проведении проверок НДС в части ст. 54.1) не менее...',
-            'city': 'Москва'}]
+                                   'name': 'Главный государственный налоговый инспектор отдела выездных проверок, предпроверочного анализа',
+                                   'salary': {'from': 80000, 'to': None, 'currency': 'RUR', 'gross': True},
+                                   'url': 'https://hh.ru/vacancy/93226541',
+                                   'description': 'Высшее образование по направлениям подготовки: экономика, бухгалтерский учет, юриспруденция, государственное и муниципальное управление, экономическая безопасность. Опыт работы в ИФНС.',
+                                   'city': 'Санкт-Петербург'},
+                                  {'id': '92182049',
+                                   'name': 'Старший государственный налоговый инспектор контрольно-аналитического отдела',
+                                   'salary': {'from': 80000, 'to': 100000, 'currency': 'RUR', 'gross': True},
+                                   'url': 'https://hh.ru/vacancy/92182049',
+                                   'description': 'опыт работы в контрольном блоке (КАО, ППА, ВНП, ОКП при проведении проверок НДС в части ст. 54.1) не менее...',
+                                   'city': 'Москва'}]
 
 
 def test_certain_vacancy():
@@ -45,3 +47,19 @@ def test_certain_vacancy():
     v = json.loads(c.full_info())
     assert v["name"] == "Бухгалтер"
     assert v["id"] == '7777'
+
+
+# import sys
+# from io import StringIO
+#
+#
+# def test_main_func(capsys):
+#     captured_output = StringIO()
+#     sys.stdout = captured_output
+#
+#     for_usr(4)
+#
+#     sys.stdout = sys.__stdout__
+#
+#     captured = captured_output.getvalue().strip()
+#     assert captured == "Указан некорректный номер запроса"
